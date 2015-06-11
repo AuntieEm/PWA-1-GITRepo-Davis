7a51c0e4e5c545a4d0cad78791cbcba506e1c2d6
AuntieEm <Emily.davis.5828@gmail.com>
2015-06-11 01:51:43 -0500

    Goal 2 - More: Conditionals (switch())
    - “if” conditionals are perfect for comparing several sets of varying
    conditions
    	- switch statements will evaluate a single conditional expression and
    		then perform an equality check against possible cases

--- PWA-1-Wk2/goal2_practice/js/main.js
+++ PWA-1-Wk2/goal2_practice/js/main.js
@@ -380,6 +380,75 @@ console.log('------ MORE Operators - typeof() ----------');
 
 console.log('------ MORE Conditionals - Switch ----------');
 
+    var fruit = "Oranges";
+
+    switch(fruit){
+        case "Oranges":
+            console.log("Oranges are $0.59 a pound.");
+            break;
+        case "Apples":
+            console.log("Apples are $0.32 a pound.");
+            break;
+        case "Bananas":
+            console.log("Bananas are $0.48 a pound.");
+            break;
+        case "Cherries":
+            console.log("Cherries are $3.00 a pound.");
+            break;
+        case "Mangoes":
+        case "Papayas":
+            console.log("Mangoes and papayas are $2.79 a pound.");
+            break;
+        default:
+            console.log("Sorry, we are out of " + fruit + ".");
+    };
+
+    var fruit = "Chicken";
+
+    switch(fruit){
+        case "Oranges":
+            console.log("Oranges are $0.59 a pound.");
+            break;
+        case "Apples":
+            console.log("Apples are $0.32 a pound.");
+            break;
+        case "Bananas":
+            console.log("Bananas are $0.48 a pound.");
+            break;
+        case "Cherries":
+            console.log("Cherries are $3.00 a pound.");
+            break;
+        case "Mangoes":
+        case "Papayas":
+            console.log("Mangoes and papayas are $2.79 a pound.");
+            break;
+        default:
+            console.log("Sorry, we are out of " + fruit + ".");
+    };
+
+    var fruit = "Mangoes"; //case sensitive     //Output:  Mangoes and papayas are..
+
+    switch(fruit){
+        case "Oranges":
+            console.log("Oranges are $0.59 a pound.");
+            break;
+        case "Apples":
+            console.log("Apples are $0.32 a pound.");
+            break;
+        case "Bananas":
+            console.log("Bananas are $0.48 a pound.");
+            break;
+        case "Cherries":
+            console.log("Cherries are $3.00 a pound.");
+            break;
+        case "Mangoes":
+        case "Papayas":
+            console.log("Mangoes and papayas are $2.79 a pound.");
+            break;
+        default:
+            console.log("Sorry, we are out of " + fruit + ".");
+    };
+
 /*******************************************
  STUDENT ACTIVITY 6:
 
